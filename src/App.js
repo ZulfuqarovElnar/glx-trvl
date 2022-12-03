0a332f65c4dab1e481d9a6c9ce3fd27dbb3d7200
Elnar <elnarzulfuqarli2001@gmail.com>
2022-12-03 12:54:37 +0400

    first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React from 'react'
-import { Routes, Route} from "react-router-dom";
 
-import Home from './routes/Home';
 
 function App() {
   return (
-   <>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-   </>
+    <div >
+     
+    </div>
   );
 }
 

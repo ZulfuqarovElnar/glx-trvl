1e19bd89c7e2f5a7abcf148834c3ce01406acac7
Elnar <elnarzulfuqarli2001@gmail.com>
2022-12-06 14:18:19 +0400

    Contact sayfasi, Style eklendi ve son duzelisler

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,26 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom'
-import {FaBars, FaTimes} from 'react-icons/fa'
-
+import {FaBars, FaTimes} from 'react-icons/fa';
 import './NavbarStyles.css';
 
 const Navbar = () => {
     const[click, setClick] = useState(false)
     const handleClick = () => setClick(!click)
 
+const [color, setColor] = useState(false)
+    const changeColor = () => {
+        if (window.scrollY >= 100) {
+            setColor(true)
+        } else {
+            setColor(false)
+        }
+    }
 
+    window.addEventListener('scroll', changeColor)
 
 
   return (
-    <div className='header'>
+    <div className={color ? 'header header-bg' : 'header'}>
         <Link to='/'><h1>GLX TRVL</h1></Link>
         <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li>
